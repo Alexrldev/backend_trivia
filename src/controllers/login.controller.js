602d823b8b6177aaade0448a1fc595e1732e0c76
Alexrldev <118137925+Alexrldev@users.noreply.github.com>
2024-11-14 22:54:11 -0600

    Update login.controller.js parametros cookie

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -18,8 +18,8 @@ export const registroUsu= async(req, res)=>
 
         const token= await createAccesToken({id:usuarioLoginSaved._id});
         //nombre de la cookie, su info
-        res.setHeader("Set-Cookie",["SameSite=none","secure"])
-        res.cookie("token",token);
+       
+        res.cookie("token",token, {httpOnly:true, sameSite:none, secure:true});
         res.send("Usuario creado");
 
        /* jwt.sign({id:usuarioLoginSaved._id}, "secret",{expiresIn:"1d"}, (err,token)=>
